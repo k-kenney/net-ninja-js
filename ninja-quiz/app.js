c18bc265f125893ee784a7ddb7c43a4f3ad3dec6
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-07-23 10:29:17 +0300

    completed ninja quiz with setInterval method

--- ninja-quiz/app.js
+++ ninja-quiz/app.js
@@ -16,8 +16,19 @@ form.addEventListener('submit', e => {
   });
   // show result on page
   scrollTo(0,0);
-  result.querySelector('span').textContent = `${score}%`;
+
   result.classList.remove('d-none');
+
+  let output = 0;
+  const timer = setInterval(() => {
+    result.querySelector('span').textContent = `${output}%`;
+    if(output === score){
+      clearInterval(timer);
+    } else {
+      output++;
+    }
+  }, 10);
+
 });
 
 // window object
@@ -25,3 +36,12 @@ form.addEventListener('submit', e => {
 // setTimeout(() => {
 //   alert('hello, ninjas')
 // }, 3000);
+
+// let i = 0;
+// const timer = setInterval(() => {
+//   console.log('hello');
+//   i++;
+//   if(i === 5) {
+//     clearInterval(timer)
+//   }
+// }, 1000);
