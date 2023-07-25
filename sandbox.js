ceccfb24facda4bc3c3b7fee6bea77f9f1928d6f
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-07-25 09:10:03 +0300

    created todo list in JS

--- sandbox.js
+++ sandbox.js
@@ -1,27 +1,45 @@
-const form = document.querySelector('.signup-form');
-const feedback = document.querySelector('.feedback');
-const usernamePattern = /^[a-zA-Z]{6,12}$/;
+// const names = ['mario', 'shaun', 'chun-li', 'yoshi', 'luigi'];
+// // names.sort();
+// // names.reverse();
+// console.log(names);
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
+// const scores = [10, 5, 0, 40, 30, 10, 20, 70];
+// // scores.reverse();
+// // console.log(scores);
 
-  const username = form.username.value;
+const products = [
+  {name: 'gold star', price: 30},
+  {name: 'green shell', price: 10},
+  {name: 'red shell', price: 50},
+  {name: 'banana skin', price: 5},
+  {name: 'mushroom', price: 50},
+];
 
-  if(usernamePattern.test(username)) {
-    feedback.textContent = 'that username is valid!'
-  } else {
-    feedback.textContent = 'username must containe letters only and be between 6 and 12 characters long';
-  }
+// const filtered = products.filter(product => product.price > 20);
 
-});
+// const promos = filtered.map(product => {
+//   return `the ${product.name} is ${product.price / 2} pounds`;
+// })
 
+const promos = products
+.filter(product => product.price > 20)
+.map(product => `the ${product.name} is ${product.price / 2} pounds`);
 
-form.username.addEventListener('keyup', e => {
-  console.log(e);
-  // console.log(e.target.value, form.username.value);
-  if(usernamePattern.test(e.target.value)) {
-    form.username.setAttribute('class', 'success');
-  } else {
-    form.username.setAttribute('class', 'error');
-  }
-});
+console.log(promos);
+
+// scores.sort((a,b) => a - b);
+// console.log(scores);
+
+// players.sort((a,b) => {
+//   if(a.score > b.score) {
+//     return -1;
+//   } else if(b.score > a.score) {
+//     return 1;
+//   } else {
+//     return 0;
+//   }
+// });
+
+// players.sort((a,b) => b.score - a.score);
+
+// console.log(players);
